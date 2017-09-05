f83e99f34b70f53f0602e386f00f85947f22b710
Connor Dean <connor.dean@washburn.edu>
2017-09-05 18:18:39 -0500

    Initial setup

---
+++ CCC_WuMap/app/src/main/java/com/example/connor/ccc_wumap/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.connor.ccc_wumap;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                //changing to Map
+                /*
+                TODO
+                String: title_map
+                ID: navigation_map
+                 */
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                //changing to Buildings
+                /*
+                TODO
+                String: title_buildings
+                ID: navigation_buildings
+                 */
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                //changing to Departments
+                /*
+                TODO
+                String: title_departments
+                ID: navigation_departments
+                 */
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
